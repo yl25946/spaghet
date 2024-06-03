1e29e3b70f9bbe1cd3c84d6e4a25f902d041937b
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-06-03 10:09:02 -0500

    reserved a vector size and changed it to twofold
    BENCH: 5611138

--- src/search.h
+++ src/search.h
@@ -24,7 +24,7 @@ class Searcher
 {
 public:
     // zorbrist hashes
-    std::vector<uint64_t> threefold_repetition;
+    std::vector<uint64_t> game_history;
 
     // don't really change this lol, just for reference
     Board &board;
@@ -69,7 +69,7 @@ public:
 
     // checks if there's a threefold draw
     // returns true if there is a draw
-    bool threefold(Board &board);
+    bool twofold(Board &board);
 
     void bench();
 };
