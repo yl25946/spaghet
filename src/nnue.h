6c5fd1f5975e015974c02e5f1207f2ce688d3404
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-06-17 03:01:26 -0500

    started writing inference

---
+++ src/nnue.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "defs.h"
+
+constexpr int INPUT_WEIGHTS = 768;
+constexpr int HIDDEN_SIZE = 1024;
+using NNUE_indices = std::pair<std::size_t, std::size_t>;
+
+class Network
+{
+    int16_t feature_weights[INPUT_WEIGHTS][HIDDEN_SIZE];
+    int16_t feature_bias[HIDDEN_SIZE];
+    int16_t output_weights[HIDDEN_SIZE][2];
+    int16_t output_bias;
+};
+
+extern Network net;
+
+class NNUE
+{
+}
