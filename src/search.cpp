b1cafe47ecbe82aaa08535e8168b192fac491766
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-01 16:58:42 -0500

    yolo
    BENCH: 3763545

--- src/search.cpp
+++ src/search.cpp
@@ -491,6 +491,9 @@ int Searcher::negamax(int alpha, int beta, int depth, bool cutnode, SearchStack 
         if (cutnode)
             reduction += 1;
 
+        if (!tt_move.is_quiet())
+            reduction += 2;
+
         // Late Move Reduction: we've ordered the move in order of importance. We reduce the
         // the depths of later moves because they are less important
         if (move_picker.moves_seen() > 1)
