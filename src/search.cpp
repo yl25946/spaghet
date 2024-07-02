8971c1bf13f6752511b4feea98fa9fbc1380ada0
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-01 21:38:47 -0500

    we ball
    BENCH: 11371906

--- src/search.cpp
+++ src/search.cpp
@@ -454,7 +454,9 @@ int Searcher::negamax(int alpha, int beta, int depth, bool cutnode, SearchStack 
                 // No move was able to beat the TT entries score, so we extend the TT
                 // move's search
                 if (singular_score < singular_beta)
-                    ++extensions;
+                {
+                    extensions += 2;
+                }
 
                 // Multicut: Since the sigular search failed high, that means that the main search is likely to fail high too, so if our singular_beta
                 // is larger than beta, we can cutoff
