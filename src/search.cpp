b4ace6d03df44eb7fb1a43219eab487c574e9895
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-03 20:33:44 -0500

    take 3
    BENCH: 3455029

--- src/search.cpp
+++ src/search.cpp
@@ -483,9 +483,9 @@ int Searcher::negamax(int alpha, int beta, int depth, bool cutnode, SearchStack 
                 // move's search
                 if (singular_score < singular_beta)
                 {
-                    int double_margin = 290 * inPV - 200 * !tt_move.is_capture();
+                    // int double_margin = 290 * inPV - 200 * !tt_move.is_capture();
 
-                    extensions += 1 + (singular_score < singular_beta - double_margin);
+                    extensions += 1 + (!inPV && singular_score < singular_beta - 100);
                 }
 
                 // Multicut: Since the sigular search failed high, that means that the main search is likely to fail high too, so if our singular_beta
