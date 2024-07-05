a22f3b6a78e78eee190cfd6f214a5155cf544ad4
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-05 15:39:44 -0500

    I'm lowkey stupid
    BENCH: 3143446

--- src/search.cpp
+++ src/search.cpp
@@ -499,8 +499,8 @@ int Searcher::negamax(int alpha, int beta, int depth, bool cutnode, SearchStack 
                     extensions -= 2;
 
                 // if we're in a cut node, we expect it to fail high, so we can reduce the depth using a negative extension
-                else if (cutnode)
-                    extensions -= 1;
+                // else if (cutnode)
+                //     extensions -= 1;
             }
         }
 
