74a9aa0abc412e150674d423f2554427f1762563
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-07-16 15:03:11 -0500

    quant net!

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-NETWORK_NAME = 32.bin
+NETWORK_NAME = 768.bin
 _THIS       := $(realpath $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
 _ROOT       := $(_THIS)
 EVALFILE     = $(NETWORK_NAME)
--- src/nnue.h
+++ src/nnue.h
@@ -6,7 +6,7 @@
 #include "incbin/incbin.h"
 
 constexpr int INPUT_WEIGHTS = 768;
-constexpr int HIDDEN_SIZE = 32;
+constexpr int HIDDEN_SIZE = 768;
 constexpr int SCALE = 400;
 constexpr int L1Q = 255;
 constexpr int OutputQ = 64;
