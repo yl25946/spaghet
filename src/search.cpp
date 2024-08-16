cdb0693bc1ec5a616a48d2f167b8cfe8aa77803a
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-08-16 18:03:01 -0500

    reduce more if we're improving
    BENCH: 1991829

--- src/search.cpp
+++ src/search.cpp
@@ -543,7 +543,7 @@ int Searcher::negamax(int alpha, int beta, int depth, bool cutnode, SearchStack 
         if (curr_move == ss->killers[0] || curr_move == ss->killers[1])
             --reduction;
 
-        if (!improving)
+        if (improving)
             ++reduction;
 
         if (is_quiet)
