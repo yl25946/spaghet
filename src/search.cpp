99246dc39318d5a70a7391b455c2bb90e36a976c
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-08-25 23:38:11 -0500

    more fun kek
    BENCH: 3940011

--- src/search.cpp
+++ src/search.cpp
@@ -297,7 +297,7 @@ int Searcher::negamax(int alpha, int beta, int depth, bool cutnode, SearchStack 
     (ss + 1)->killers.clear();
 
     // applies null move pruning
-    if (!(ss - 1)->null_moved && !inPV && !ss->exclude_tt_move && !ss->in_check && !board.only_pawns(board.side_to_move) && ss->static_eval >= beta)
+    if (!(ss - 1)->null_moved && !inPV && !ss->exclude_tt_move && !ss->in_check && !board.only_pawns(board.side_to_move) && eval >= beta && ss->static_eval >= beta - 20 * depth + 400)
     {
         int r = std::min((eval - beta) / 200, 6) + depth / 3 + 4;
 
