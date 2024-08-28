71323e2649c0aa761bd83850d19c8470629cbd4b
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-08-28 12:50:20 -0500

    take 2
    BENCH: 3286813

--- src/board.cpp
+++ src/board.cpp
@@ -244,7 +244,7 @@ uint64_t Board::material_hash() const
 {
     uint64_t material_hash = 0;
     uint64_t piece_bitboard;
-    for (int piece = WHITE_KNIGHT; piece <= BLACK_QUEEN; ++piece)
+    for (int piece = WHITE_PAWN; piece <= BLACK_QUEEN; ++piece)
     {
         piece_bitboard = bitboard(piece);
         material_hash ^= zobrist_pieces[piece][count_bits(piece_bitboard)];
