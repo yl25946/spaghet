a26cdc5ccc8f5fe75ea303696950fd7c4138ab8e
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2024-09-08 21:17:32 -0500

    huh
    BENCH: 2832455

--- src/movepicker.cpp
+++ src/movepicker.cpp
@@ -5,14 +5,16 @@ int64_t get_quiet_history_score(SearchStack *ss, ThreadData &thread_data, Move q
 
     int ply = ss->ply;
 
-    int64_t quiet_move_score = thread_data.main_history.move_value(quiet_move, ss->board.side_to_move);
+    int64_t quiet_move_score = thread_data.main_history.move_value(quiet_move, ss->board.side_to_move) / 2;
 
     quiet_move_score += thread_data.pawnhist.move_value(ss->board, quiet_move);
 
     // how many moves we look backwards
-    for (int conthist_index : conthist_indices)
-        if (ply >= conthist_index && !(ss - conthist_index)->null_moved)
-            quiet_move_score += thread_data.conthist.move_value(ss->board, quiet_move, (ss - conthist_index)->board, (ss - conthist_index)->move_played);
+    if (ply >= 1 && !(ss - 1)->null_moved)
+        quiet_move_score += thread_data.conthist.move_value(ss->board, quiet_move, (ss - 1)->board, (ss - 1)->move_played);
+
+    if (ply >= 2 && !(ss - 2)->null_moved)
+        quiet_move_score += thread_data.conthist.move_value(ss->board, quiet_move, (ss - 2)->board, (ss - 2)->move_played);
 
     return quiet_move_score;
 }
